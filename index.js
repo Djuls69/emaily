dccdcec328d6a4a034c1b7efdb524cc19b54172d
Falk <julien.delusseau@gmail.com>
2020-10-02 16:27:31 +0200

    First commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+  res.json({ hi: 'there' })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log('Server started ...'))
